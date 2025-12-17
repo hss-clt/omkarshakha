b518f546858e6cdfbf6e7963888b986e71f00c92
anupambaburaj <anupambaburaj@gmail.com>
2025-12-17 18:53:44 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+// Define the Header Content
+const headerContent = `
+    <header>
+        <nav>
+            <a href="index.html">Home</a>
+            <a href="balagokulam.html">Balagokulam</a>
+            <a href="surya_namaskar.html">Surya Namaskar</a>
+            <a href="sewa.html">Sewa</a>
+            <a href="report.html">Report</a>
+        </nav>
+    </header>
+`;
+
+// Define the Footer Content
+const footerContent = `
+    <footer>
+        <div class="footer-info">
+            <p>Contact us: <a href="tel:+19803692983,1">+1 (980) 369-2983 ext:1</a></p>
+            <p>Follow us: <a href="https://www.facebook.com/cltomkar/" target="_blank">Facebook</a></p>
+            <p>&copy; 2025 Omkar Shakha All rights reserved.</p>
+        </div>
+    </footer>
+`;
+
+// Insert into the page
+document.getElementById('header-placeholder').innerHTML = headerContent;
+document.getElementById('footer-placeholder').innerHTML = footerContent;
